6335b1a24ccff362ae15ef18433aaa3b208d51d5
Navanee <navaneekrish164@gmail.com>
2022-08-07 18:51:27 +0530

    tags

---
+++ practice-1/main.js
@@ -0,0 +1,16 @@
+let text = document.getElementById("name");
+let password = document.getElementById("password");
+
+function myfunction(){
+
+      name.text=name.value
+      if(1<8){
+        password.nextparrentNode
+      }else{
+        console.log("error");
+      }
+      if
+
+}
+
+
